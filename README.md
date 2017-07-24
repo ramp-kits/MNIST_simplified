0f84fd65d17161d4117bc3c2c4d2ae2645345d16
Balazs Kegl <balazs.kegl@gmail.com>
2017-07-24 14:11:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# RAMP starting kit on the MNIST dataset
+
+Authors: Mehdi Cherti & Balazs Kegl
+
+[![Build Status](https://travis-ci.org/ramp-kits/MNIST.svg?branch=master)](https://travis-ci.org/ramp-kits/MNIST)
+
+Go to [`ramp-worflow`](https://github.com/paris-saclay-cds/ramp-workflow) for more help on the [RAMP](http:www.ramp.studio) ecosystem.
+
+After cloning, run
+
+```
+python download_data.py
+```
+
+the first time. It will create `data/imgs` and download the images there
+using the names `<id>.png`, where `<id>`s are coming from `data/train.csv` and `data/test.csv`.
+
+
+Install ramp-workflow (rampwf), then execute
+
+```
+ramp_test_submission
+```
+
+Get started on this RAMP with the [dedicated notebook](MNIST_starting_kit.ipynb).
---
+++ download_data.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import numpy as np
+import pandas as pd
+from subprocess import call
+from skimage.io import imsave
+
+
+def convert(ids, X, out_img_folder):
+    for id_, x in zip(ids, X):
+        imsave('{}/{}.png'.format(out_img_folder, id_), x)
+
+url = 'https://s3.amazonaws.com/img-datasets/mnist.npz'
+print('Downloading {} ...'.format(url))
+f_name = os.path.basename(url)
+if not os.path.exists(f_name):
+    call('wget {}'.format(url), shell=True)
+f = np.load(f_name)
+X_train = f['x_train']
+df_train = pd.read_csv(os.path.join('data', 'train.csv'))
+df_test = pd.read_csv(os.path.join('data', 'test.csv'))
+ids_train = df_train['id'].values
+ids_test = df_test['id'].values
+ids = np.concatenate([ids_train, ids_test])
+
+imgs_dir = os.path.join('data', 'imgs')
+print('Saving images in {}/<id>.png ...'.format(imgs_dir))
+if os.path.exists(imgs_dir):
+    shutil.rmtree(imgs_dir)
+os.mkdir(imgs_dir)
+for id, x in zip(ids, X_train):
+    imsave('{}/{}.png'.format(imgs_dir, id), x)
